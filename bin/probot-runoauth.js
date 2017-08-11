c6a564e42340539deafeea5bd04bddae6c89f1e8
Jamie Jones <jbjonesjr@github.com>
2017-08-10 23:37:48 -0400

    bringing oauth apps into the correct style and options (for now)

--- bin/probot-runoauth.js
+++ bin/probot-runoauth.js
@@ -9,17 +9,19 @@ program
   .usage('[options] <plugins...>')
   .option('-p, --port <n>', 'Port to start the server on', process.env.PORT || 3000)
   .option('-t, --tunnel <subdomain>', 'Expose your local bot to the internet', process.env.SUBDOMAIN || process.env.NODE_ENV !== 'production')
-  .option('-o, --oauthtoken <token>', 'OAuth token for your application', process.env.token)
-  .option('-s, --secret <secret>', 'Webhook secret of the OAuth App', process.env.WEBHOOK_SECRET)
+  .option('-o, --oauthtoken <token>', 'OAuth token for your application', process.env.TOKEN)
+  .option('-c, --clientid <id>', 'OAuth token for your application', process.env.CLIENT_ID)
+  .option('-s, --secret <secret>', 'Webhook secret of the GitHub App', process.env.WEBHOOK_SECRET)
+  .option('-s, --clientsecret <secret>', 'OAuth token for your application', process.env.CLIENT_SECRET)
   .parse(process.argv);
 
 process.env.AUTH_METHOD = 'oauth';
 program.app = program.integration;
 
-if (!program.oauthtoken) {
+/* If (!program.oauthtoken) {
   console.warn('Missing GitHub OAuth Token.\nUse --oauthtoken flag or set token environment variable.');
   program.help();
-}
+} */
 
 if (program.tunnel) {
   try {
@@ -36,7 +38,11 @@ if (program.tunnel) {
 
 const createProbot = require('../');
 
-const probotopts = {port: program.port};
+const probotopts = {port: program.port,
+  clientid: program.clientid,
+  clientsecret: program.clientsecret,
+  secret: program.secret
+};
 
 const probot = createProbot(probotopts);
 
