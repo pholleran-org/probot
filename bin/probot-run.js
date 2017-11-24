853fa30901c49034019208c079a492de438074fb
Jamie Jones <jbjonesjr@github.com>
2017-11-24 00:00:41 -0500

    update formatting of the private key

--- bin/probot-run.js
+++ bin/probot-run.js
@@ -5,9 +5,7 @@ require('dotenv').config()
 const pkgConf = require('pkg-conf')
 const program = require('commander')
 
-const {
-  findPrivateKey
-} = require('../lib/private-key')
+const { findPrivateKey } = require('../lib/private-key')
 
 program
   .usage('[options] <apps...>')
